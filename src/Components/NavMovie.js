80d57f5a7c0cda6bb5f140036e42002be26596b4
rania-e <elhechemirania@gmail.com>
2024-12-23 12:58:16 +0100

    first commit

---
+++ src/Components/NavMovie.js
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const NavMovie=()=>{
+    return(
+    <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Movie App</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Movies</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    )
+}
+
+export default NavMovie
